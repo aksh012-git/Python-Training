e1ced8eeb7d49d84d4e99e361e3b1ec15a4ffd8e
Aksh <akshm.wot2022@gmail.com>
2022-03-04 11:27:22 +0530

    adding p19.py

--- Data_Structure/P19.py
+++ Data_Structure/P19.py
@@ -7,8 +7,8 @@
 
 # Ans = 4
 
-r1_1 = [2,6]
-r1_2 = [6,2]
+r1_1 = [2,2]
+r1_2 = [6,6]
 
 r2_1 = [4,4]
 r2_2 = [8,8]
@@ -16,7 +16,7 @@ r2_2 = [8,8]
 r1_slop = (r1_1[0] - r1_2[0])/(r1_1[1] - r1_2[1])
 r2_slop = (r2_1[0] - r2_2[0])/(r2_1[1] - r2_2[1])
 
-print(r1_slop,r2_slop)
+# print(r1_slop,r2_slop)
 
 r1_1_ = []
 r1_2_ = []
@@ -28,7 +28,26 @@ if r1_slop < 0:
     r1_1_.append(min(r1_1[1],r1_2[1]))
     r1_2_.append(max(r1_1[0],r1_2[0]))
     r1_2_.append(max(r1_1[1],r1_2[1]))  
+else:
+    r1_1_ = r1_1
+    r1_2_ = r1_2
+    
 if r2_slop < 0:
-    print(r2_slop)
+    r2_1_.append(min(r2_1[0],r2_2[0]))
+    r2_1_.append(min(r2_1[1],r2_2[1]))
+    r2_2_.append(max(r2_1[0],r2_2[0]))
+    r2_2_.append(max(r2_1[1],r2_2[1]))
+else:
+    r2_1_ = r2_1
+    r2_2_ = r2_2
     
     
+# print(r1_2_,r2_2_,r1_1_,r2_1_)
+    
+
+x_axis = (min(r1_2_[0], r2_2_[0]) - max(r1_1_[0], r2_1_[0]))
+y_axis = (min(r1_2_[1], r2_2_[1]) - max(r1_1_[1], r2_1_[1]))
+
+# print(x_axis,y_axis)
+
+print(x_axis*y_axis)
--- Playground/test.py
+++ Playground/test.py
@@ -1,22 +1,33 @@
-# import datetime  
-# birthday=input("What is your next B'day Date? (in DD/MM/YYYY) ")  
-# birthdate=datetime.datetime.strptime(birthday,"%d/%m/%Y").date()
-# print(type(birthdate.date),birthdate.data())
+# Python program to find total area of two
+# overlapping Rectangles
+# Returns Total Area of two overlap
+# rectangles
 
 
-# from datetime import date
+def overlappingArea(l1, r1, l2, r2):
+	x = 0
+	y = 1
+	x_dist = (min(r1[x], r2[x]) -
+			max(l1[x], l2[x]))
 
-# today = date.today().year
-# print(type(today.year))
-
-
-try:
-    x = 0
-    try:
-        r = 0
-    except Exception:
-        w = 0
-    c = 232
+	y_dist = (min(r1[y], r2[y]) -
+			max(l1[y], l2[y]))
     
-except Exception:
-    c = 0
+	areaI = 0
+	if x_dist > 0 and y_dist > 0:
+		areaI = x_dist * y_dist
+
+	print(r1,r2,l1,l2,areaI,x_dist,y_dist)
+
+
+
+# Driver's Code
+l1 = [3, 2]
+r1 = [6, 5]
+l2 = [5, 4]
+r2 = [8, 7]
+
+# Function call
+print(overlappingArea(l1, r1, l2, r2))
+
+# This code is contributed by Manisha_Ediga
