d1517de9766f440da0236836ecac8680abc4c598
Aksh <akshm.wot2022@gmail.com>
2022-03-15 10:46:09 +0530

    complete pandas

--- 11_Pandas/P2_operation_over_ufo_sighting_data.py
+++ 11_Pandas/P2_operation_over_ufo_sighting_data.py
@@ -6,16 +6,17 @@ ufo_data = pd.read_csv('/home/wot-aksh/Desktop/Python_Training/11_Pandas/ufo_sig
 #----------------------------------------------------------------------------------------------------------------------------
 """Write a Pandas program to create today's date"""
 
-print(pd.datetime.now())
+print('Current date and time: ',pd.datetime.now(),'\n')
 
 
 #----------------------------------------------------------------------------------------------------------------------------
 """Write a Pandas program to calculate all the sighting days of the unidentified flying object (ufo) from the current date."""
+
 end_date = '10/11/2005'
 
 ufo_data['Date_time'] = ufo_data['Date_time'].str.replace('24:00','0:00')
 
 ufo_data['Date_time'] = pd.to_datetime(ufo_data['Date_time']).dt.date
 
-print(ufo_data.loc[ufo_data['Date_time'] <= pd.to_datetime(end_date)])
+print('ufo appeared {0} days till date {1}'.format(ufo_data.loc[ufo_data['Date_time'] <= pd.to_datetime(end_date)].shape[0],end_date))
 
