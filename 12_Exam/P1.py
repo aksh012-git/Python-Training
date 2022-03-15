2106b0f30738776c31966acaac149ce41d01cf30
Aksh <akshm.wot2022@gmail.com>
2022-03-15 19:25:44 +0530

    update exam module

--- 12_Exam/P1.py
+++ 12_Exam/P1.py
@@ -1,12 +1,24 @@
-num = 99
-x = 0
-while int(num/10) != 0:
-    x = 0
-    while num != 0:
-        x += num%10
-        num  = int(num/10)
-    num = x   
+# Finding the sum of all digits of a number until the sum becomes a single digit.
+# Examples. 10589=>5 , 121=>4, 99=>9, 10=>1
 
-print(x)
+input_number = 1234567
+sum_of_model_of_input = 0
 
+while int(input_number/10) != 0:
+    sum_of_model_of_input = 0
+    while input_number != 0:
+        sum_of_model_of_input += input_number%10
+        input_number  = int(input_number/10)
+    input_number = sum_of_model_of_input   
 
+print(sum_of_model_of_input)
+
+#----------------------------------------------------------------
+
+if input_number % 9 == 0:
+    print(9)
+else:
+    print(input_number%9)
+    
+
+    
